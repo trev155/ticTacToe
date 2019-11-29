e0e470cc63ff322a8090335e808207e00eb75d3d
trev155 <trevku155@gmail.com>
2019-11-29 10:35:33 -0500

    React Tutorials Exercise 3 - Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square 
+        key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
        />
@@ -21,24 +22,17 @@ class Board extends React.Component {
   }
 
   render() {
+    let grid = []
+    for (let row = 0; row < 3; row++) {
+      let squaresRow = [];
+      for (let col = 0; col < 3; col++) {
+        squaresRow.push(this.renderSquare((row * 3) + col));
+      }      
+      grid.push(<div key={row} className='board-row'>{squaresRow}</div>);
+    }
+
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <div>{grid}</div>
     );
   }
 }
@@ -95,7 +89,7 @@ class Game extends React.Component {
       let desc = move ?
         'Go to move #' + move + " - " + JSON.stringify(movePosition) :
         'Go to game start';
-      if (this.state.stepNumber == move) {
+      if (this.state.stepNumber === move) {
         desc = <b>{desc}</b>
       }
 
